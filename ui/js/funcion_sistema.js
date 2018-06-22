bb1f88a63da25cb3370ad9361593325399cf1bc4
Alexis <alexisjmp@gmail.com>
2018-06-22 10:38:30 -0400

    actuliza funciones de calendar

--- ui/js/funcion_sistema.js
+++ ui/js/funcion_sistema.js
@@ -463,6 +463,38 @@ function login_menu() {
     parametros = "";
     sendajax_modal(marco, ruta, data);
 }
+$( function() {
+    // alert("fdsafdsf");
+  $( "#datepicker_inicio" ).datepicker();
+  } ); 
+  $( function() {
+    $( "#datepicker_fin" ).datepicker();
+  } );
+  $( function() {
+    // alert("fdsafdsf");
+  $( "#datepicker_rpinicio" ).datepicker();
+  
+  $( "#datepicker_rpfin" ).datepicker();
+  } );//23-12-2017 hecho por el pana 
+  
+   $.datepicker.regional['es'] = {
+ closeText: 'Cerrar',
+ prevText: '< Ant',
+ nextText: 'Sig >',
+ currentText: 'Hoy',
+ monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+ monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+ dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+ dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sab'],
+ dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sa'],
+ weekHeader: 'Sm',
+ dateFormat: 'dd/mm/yy',
+ firstDay: 1,
+ isRTL: false,
+ showMonthAfterYear: false,
+ yearSuffix: ''
+ };
+ $.datepicker.setDefaults($.datepicker.regional['es']);
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
